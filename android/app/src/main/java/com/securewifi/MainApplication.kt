63ecb35a85cee67f4baac8195fb26a6207db5448
OscarGomezGonzalezz <oscgomgon.business@gmail.com>
2025-02-16 19:30:16 +0100

    Descripción de los cambios

--- android/app/src/main/java/com/securewifi/MainApplication.kt
+++ android/app/src/main/java/com/securewifi/MainApplication.kt
@@ -1,6 +1,9 @@
 package com.securewifi
 
 import android.app.Application
+import android.content.Intent
+import android.os.Build
+import android.provider.Settings
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -18,8 +21,7 @@ class MainApplication : Application(), ReactApplication {
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
+              // Aquí puedes agregar paquetes manualmente si es necesario.
             }
 
         override fun getJSMainModuleName(): String = "index"
@@ -36,9 +38,16 @@ class MainApplication : Application(), ReactApplication {
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, OpenSourceMergedSoMapping)
+
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
+    }
+
+    // Para Android 10+ abrir la configuración de Wi-Fi automáticamente
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      val intent = Intent(Settings.ACTION_WIFI_SETTINGS)
+      intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+      startActivity(intent)
     }
   }
 }
